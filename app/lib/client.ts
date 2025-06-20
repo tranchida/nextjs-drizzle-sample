ed46118d3cb361e9b78edde98fc604f08fdee4c0
Giampaolo <giampaolo.tranchida@gmail.com>
2025-06-20 22:00:30 +0200

    Ajout de la dépendance @neondatabase/serverless et mise à jour de la configuration de la base de données dans client.ts pour utiliser le client Neon. Suppression de l'ancienne logique de connexion avec Pool.

--- app/lib/client.ts
+++ app/lib/client.ts
@@ -1,25 +1,7 @@
-import { drizzle } from "drizzle-orm/node-postgres"
-import { Pool } from "pg"
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import 'dotenv/config'; 
 
-// Déclare une propriété sur l'objet global pour TypeScript
-declare global {
-  // eslint-disable-next-line no-var
-  var pool: Pool | undefined;
-}
+const sql = neon(process.env.DATABASE_URL!);
 
-const pool =
-  global.pool ||
-  new Pool({
-    connectionString: process.env.DATABASE_URL!,
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  global.pool = pool;
-}
-
-const db = drizzle(pool, { logger: true });
-
-console.log(process.env.DATABASE_URL);
-
-export default db;
+export const db = drizzle({ client: sql });
